8bc90ff24430bee18dc02ba51e6605ee842b6c5c
d.koshelev <d.koshelev@zebrains.team>
2024-07-07 19:26:32 +0300

    ..

--- effects/effect2.py
+++ effects/effect2.py
@@ -10,20 +10,20 @@ class Effect2(BaseEffect):
     def __init__(self) -> None:
         super().__init__()
         self.length = 20
-        self.donar_img_path = "./donar_imgs/14.png"
+        self.donor_img_path = "./donor_imgs/14.png"
 
         self._settings_dict = {
             "lenght": f"{self.length}",
-            "donar_path": f"{self.donar_img_path}",
+            "donor_path": f"{self.donor_img_path}",
         }
         self.is_ready = False
 
     def settings(self, settings_dict: dict):
 
-        self.donar_img_path = settings_dict["donar_path"]
+        self.donor_img_path = settings_dict["donor_path"]
         self.length = int(settings_dict["lenght"])
 
-        self.donar_img = cv2.imread(self.donar_img_path)
+        self.donor_img = cv2.imread(self.donor_img_path)
         mp_pose = mp.solutions.pose
         self.model = mp_pose.Pose(
             min_detection_confidence=0.5, min_tracking_confidence=0.5
@@ -62,55 +62,55 @@ class Effect2(BaseEffect):
                 mp_pose.PoseLandmark.LEFT_ANKLE.value,
             ),
         ]
-        self.pieces_of_donar_img = []
-        self.donar_points_list = []
-        self.donar_body_points_list = []
+        self.pieces_of_donor_img = []
+        self.donor_points_list = []
+        self.donor_body_points_list = []
         self.body_lanmarks = [
             mp_pose.PoseLandmark.RIGHT_SHOULDER.value,
             mp_pose.PoseLandmark.LEFT_SHOULDER.value,
             mp_pose.PoseLandmark.RIGHT_HIP.value,
             mp_pose.PoseLandmark.LEFT_HIP.value,
         ]
-        self.donar_body_img = None
+        self.donor_body_img = None
 
-        _, results = mediapipe_detection(img=self.donar_img, model=self.model)
+        _, results = mediapipe_detection(img=self.donor_img, model=self.model)
         for landmark1, landmark2 in self.landmarks:
-            donar_points = self.get_pts(
+            donor_points = self.get_pts(
                 results,
                 landmark1,
                 landmark2,
-                *self.donar_img.shape[:-1],
+                *self.donor_img.shape[:-1],
             )
             pts_for_poly = np.array(
                 [
-                    donar_points[0],
-                    donar_points[2],
-                    donar_points[3],
-                    donar_points[1],
+                    donor_points[0],
+                    donor_points[2],
+                    donor_points[3],
+                    donor_points[1],
                 ]
             )
-            mask = np.zeros(self.donar_img.shape[:-1], dtype=np.uint8)
+            mask = np.zeros(self.donor_img.shape[:-1], dtype=np.uint8)
             mask = cv2.fillPoly(mask, [pts_for_poly], 255)
-            piece_of_donar = cv2.bitwise_and(self.donar_img, self.donar_img, mask=mask)
-            self.pieces_of_donar_img.append(piece_of_donar)
-            self.donar_points_list.append(donar_points)
+            piece_of_donor = cv2.bitwise_and(self.donor_img, self.donor_img, mask=mask)
+            self.pieces_of_donor_img.append(piece_of_donor)
+            self.donor_points_list.append(donor_points)
 
         for b_l in self.body_lanmarks:
-            x = int(results.pose_landmarks.landmark[b_l].x * self.donar_img.shape[1])
-            y = int(results.pose_landmarks.landmark[b_l].y * self.donar_img.shape[0])
-            self.donar_body_points_list.append([x, y])
+            x = int(results.pose_landmarks.landmark[b_l].x * self.donor_img.shape[1])
+            y = int(results.pose_landmarks.landmark[b_l].y * self.donor_img.shape[0])
+            self.donor_body_points_list.append([x, y])
 
         pts_for_poly = np.array(
             [
-                self.donar_body_points_list[0],
-                self.donar_body_points_list[2],
-                self.donar_body_points_list[3],
-                self.donar_body_points_list[1],
+                self.donor_body_points_list[0],
+                self.donor_body_points_list[2],
+                self.donor_body_points_list[3],
+                self.donor_body_points_list[1],
             ]
         )
-        mask = np.zeros(self.donar_img.shape[:-1], dtype=np.uint8)
+        mask = np.zeros(self.donor_img.shape[:-1], dtype=np.uint8)
         mask = cv2.fillPoly(mask, [pts_for_poly], 255)
-        self.donar_body_img = cv2.bitwise_and(self.donar_img, self.donar_img, mask=mask)
+        self.donor_body_img = cv2.bitwise_and(self.donor_img, self.donor_img, mask=mask)
 
         self.is_ready = True
 
@@ -147,9 +147,9 @@ class Effect2(BaseEffect):
 
         img = paste_piece_of_img(
             img=img,
-            piece_of_img=self.donar_body_img,
+            piece_of_img=self.donor_body_img,
             in_points=person_body_pts,
-            from_points=self.donar_body_points_list,
+            from_points=self.donor_body_points_list,
         )
         for i, (landmark1, landmark2) in enumerate(self.landmarks):
             person_points = self.get_pts(
@@ -160,8 +160,8 @@ class Effect2(BaseEffect):
             )
             img = paste_piece_of_img(
                 img=img,
-                piece_of_img=self.pieces_of_donar_img[i],
+                piece_of_img=self.pieces_of_donor_img[i],
                 in_points=person_points,
-                from_points=self.donar_points_list[i],
+                from_points=self.donor_points_list[i],
             )
         return img
